074165d63cd5f634475052b92f828e4e8503578d
Bill Kracke <bill@billkracke.com>
2019-10-25 11:04:15 -0400

    adding a modal and carousel

--- js/theme.js
+++ js/theme.js
@@ -1,21 +1,25 @@
-// The following lines require CODEKIT to work. 
-// It's part of my workflow, if it isn't part of yours, 
+// The following lines require CODEKIT to work.
+// It's part of my workflow, if it isn't part of yours,
 // you will need to concatenate and minify some other way
 //
-// @codekit-prepend "../components/typography/_typography.js"
-// @codekit-prepend "../components/navigation/_navigation.js"
-
+// @codekit-prepend "../modules/navigation/_navigation.js"
 
 /* 
+   TODO: Nuke the lines below that you don't need! 
+*/
+// @codekit-prepend "../modules/modal/_modal.js"
+// @codekit-prepend "../modules/carousel/_carousel.js"
+
+/*
  * Accessible focus highlighting
  */
 
 function handleFirstTab(e) {
-  if (e.keyCode === 9) { // the "I am a keyboard user" key
-    document.body.classList.add('user-is-tabbing');
-    window.removeEventListener('keydown', handleFirstTab);
-  }
+	if (e.keyCode === 9) {
+		// the "I am a keyboard user" key
+		document.body.classList.add("user-is-tabbing");
+		window.removeEventListener("keydown", handleFirstTab);
+	}
 }
 
-window.addEventListener('keydown', handleFirstTab);
- 
+window.addEventListener("keydown", handleFirstTab);
